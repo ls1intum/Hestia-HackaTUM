2a5c510e7020ca3260b1cc9289bb80f1866d4653
Magnus Kühne <kuehne.magnus@gmail.com>
2024-11-23 15:04:27 +0100

    Add Map

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { viteEnvs } from 'vite-envs';
+export default defineConfig({
+    plugins: [
+        react(),
+        viteEnvs({
+            declarationFile: '.env',
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+});
