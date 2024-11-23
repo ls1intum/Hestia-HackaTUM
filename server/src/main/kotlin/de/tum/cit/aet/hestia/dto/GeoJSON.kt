a074cef17395665f72e5b4ff166b6e3ab859d8fa
Timor Morrien <timor.morrien@tum.de>
2024-11-23 22:41:03 +0100

    Update geo.json

--- server/src/main/kotlin/de/tum/cit/aet/hestia/dto/GeoJSON.kt
+++ server/src/main/kotlin/de/tum/cit/aet/hestia/dto/GeoJSON.kt
@@ -1,16 +1,21 @@
 package de.tum.cit.aet.hestia.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class GeoJSON(
     val type: String, // "FeatureCollection"
     val features: List<Feature>
 )
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Feature(
     val type: String, // "Feature"
     val geometry: Geometry,
     val properties: Map<String, Any>? // Key-value pairs for additional data
 )
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Geometry(
     val type: String, // Geometry type, e.g., "Point", "Polygon"
     val coordinates: Any // Coordinates array, could be List<Double>, List<List<Double>>, etc.
