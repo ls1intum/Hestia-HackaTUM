324d36686329d8f3b43201499ec1eb74160a33a5
Timor Morrien <timor.morrien@tum.de>
2024-11-24 01:35:55 +0100

    Improve parallel processing

--- server/src/main/kotlin/de/tum/cit/aet/hestia/service/ScoreService.kt
+++ server/src/main/kotlin/de/tum/cit/aet/hestia/service/ScoreService.kt
@@ -10,6 +10,7 @@ import kotlin.math.cos
 import kotlin.math.sin
 import kotlin.math.sqrt
 import kotlin.random.Random
+import kotlin.system.measureTimeMillis
 
 @ApplicationScoped
 class ScoreService {
@@ -27,38 +28,62 @@ class ScoreService {
         println("Zips: ${zips.size}")
         val interhypData = interhypService.priceIndexBuy("houses")
         println("InterhypData: ${interhypData.values.size}")
-        val zipDatas = zips.filter { interhypData.values.any { ih -> ih.zipCode == it } }.map { zip ->
-            val interhypZip = interhypData.values.first { it.zipCode == zip }
-            val prizePerSqm = interhypZip.prizePerSqm
+        val zipDatas: List<ZipData>
+        println(
+            "ZipData: ${
+                measureTimeMillis {
+                    zipDatas = zips
+                        .filter { interhypData.values.any { ih -> ih.zipCode == it } }
+                        .parallelStream()
+                        .map { zip ->
+                            val interhypZip = interhypData.values.first { it.zipCode == zip }
+                            val prizePerSqm = interhypZip.prizePerSqm
 
-            val zipLocation = googleService.getPlaceDetails(interhypZip.googlePlaceId)
+                            val zipLocation = googleService.getPlaceDetails(interhypZip.googlePlaceId)
 
-            val airQuality = googleService.getAirQuality(zipLocation)
+                            // val airQuality = googleService.getAirQuality(zipLocation)
 
-            val nearestKita = poiService.kita().map { getDistance(zipLocation, it.location) }.minOrNull() ?: 100.0
+                            val nearestKita =
+                                poiService.kita().map { getDistance(zipLocation, it.location) }.minOrNull() ?: 100.0
 
-            val nearestSchool =
-                poiService.school(zip).map { getDistance(zipLocation, it.location) }.minOrNull() ?: 100.0
+                            val nearestSchool =
+                                poiService.school(zip).map { getDistance(zipLocation, it.location) }
+                                    .minOrNull() ?: 100.0
 
-            val nearestKlinik = poiService.klinik().map { getDistance(zipLocation, it.location) }.minOrNull() ?: 100.0
+                            val nearestKlinik =
+                                poiService.klinik().map { getDistance(zipLocation, it.location) }.minOrNull() ?: 100.0
 
-            ZipData(
-                zip,
-                prizePerSqm,
-                interhypZip.googlePlaceId,
-                zipLocation,
-                0.0,
-                nearestKita,
-                nearestSchool,
-                nearestKlinik
-            )
-        }
+                            ZipData(
+                                zip,
+                                prizePerSqm,
+                                interhypZip.googlePlaceId,
+                                zipLocation,
+                                0.0,
+                                nearestKita,
+                                nearestSchool,
+                                nearestKlinik
+                            )
+                        }.toList()
+                }
+            } ms")
 
         println("ZipDatas: ${zipDatas.size}")
 
-        val distanceMatrices = zipDatas.chunked(50).parallelStream().map { chunk ->
-            googleService.getDistanceMatrix(DistanceMatrixDTO(location, chunk.map { it.placeId }, travelMode))
-        }.toList()
+        val distanceMatrices: MutableList<Map<String, Long>>
+        println(
+            "DistanceMatrices: ${
+                measureTimeMillis {
+                    distanceMatrices = zipDatas.chunked(50).parallelStream().map { chunk ->
+                        googleService.getDistanceMatrix(
+                            DistanceMatrixDTO(
+                                location,
+                                chunk.map { it.placeId },
+                                travelMode
+                            )
+                        )
+                    }.toList()
+                }
+            } ms")
 
         println("DistanceMatrices: ${distanceMatrices.size}")
         println("DistanceMatrices: ${distanceMatrices.map { it.size }}")
@@ -66,13 +91,19 @@ class ScoreService {
         val distanceMatrix = distanceMatrices.reduce { acc, map -> acc + map }
 
         val random = Random(1337)
-        val scoreMap = zipDatas.map { zipData ->
-            val distance = distanceMatrix[zipData.placeId]
+        val scoreMap: Map<String, Double>
+        println(
+            "Score: ${
+                measureTimeMillis {
+                    scoreMap = zipDatas.map { zipData ->
+                        val distance = distanceMatrix[zipData.placeId]
 
-            val score = random.nextDouble(0.0, 100.0)
+                        val score = random.nextDouble(0.0, 100.0)
 
-            zipData.zip to score
-        }.toMap()
+                        zipData.zip to score
+                    }.toMap()
+                }
+            } ms")
 
         return scoreMap
     }
